a599d8ff839ee0af230d4bddd153cfb4fba58a57
Joe Blau <josephblau@gmail.com>
2019-01-11 07:05:35 -0500

    Initial commit

---
+++ Sources/SlugifyKit.swift
@@ -0,0 +1,9 @@
+//
+//  SlugifyKit.swift
+//  SlugifyKit
+//
+//  Created by Joe Blau on 1/11/19.
+//  Copyright © 2019 SlugifyKit. All rights reserved.
+//
+
+import Foundation
---
+++ Tests/SlugifyKitTests/SlugifyKitTests.swift
@@ -0,0 +1,23 @@
+//
+//  SlugifyKitTests.swift
+//  SlugifyKit
+//
+//  Created by Joe Blau on 1/11/19.
+//  Copyright © 2019 SlugifyKit. All rights reserved.
+//
+
+import Foundation
+import XCTest
+import SlugifyKit
+
+class SlugifyKitTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        //// XCTAssertEqual(SlugifyKit().text, "Hello, World!")
+    }
+    
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
