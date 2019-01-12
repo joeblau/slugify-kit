dbb51c48923e7cf563f555ebc684a3b512e95019
Joe Blau <josephblau@gmail.com>
2019-01-11 22:04:39 -0500

    Add sluginator with test cases

--- Tests/SlugifyKitTests/SlugifyKitTests.swift
+++ Tests/SlugifyKitTests/SlugifyKitTests.swift
@@ -11,13 +11,68 @@ import XCTest
 import SlugifyKit
 
 class SlugifyKitTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        //// XCTAssertEqual(SlugifyKit().text, "Hello, World!")
+    func testRegular() {
+        XCTAssertEqual("abc", "abc".slugify)
+    }
+    
+    func testAE() {
+        XCTAssertEqual("abcae", "abcæ".slugify)
+    }
+    
+    func testOE() {
+        XCTAssertEqual("abco", "abcø".slugify)
+    }
+    
+    func testUnderscore() {
+        XCTAssertEqual("a_a", "a_a".slugify)
+    }
+    
+    func testSpecial_carrot() {
+        XCTAssertEqual("a-a", "a^a".slugify)
+    }
+    
+    func testSpecial_backtick() {
+        XCTAssertEqual("a-a", "a`a".slugify)
+    }
+    
+    func testSpecial_colon() {
+        XCTAssertEqual("a-a", "a:a".slugify)
+    }
+    
+    func testSpecial_euro() {
+        XCTAssertEqual("a-a", "a€a".slugify)
+    }
+    
+    func testSpace() {
+        XCTAssertEqual("a-a", "a a".slugify)
+    }
+    
+    func testSpace_two() {
+        XCTAssertEqual("a-a", "a  a".slugify)
+    }
+    
+    func testTile_hyphen() {
+        XCTAssertEqual("alaska-airlines-flights-back-in-the-air-after-power-outage-ktva",
+                       "Alaska Airlines flights back in the air after power outage - KTVA".slugify)
+    }
+    func testTile_comma_hyphen_colon() {
+        XCTAssertEqual("3-big-things-you-missed-in-the-markets-monday-dollar-tree-apple-roku-yahoo-finance",
+                       "3 big things you missed in the markets Monday: Dollar Tree, Apple, Roku - Yahoo Finance".slugify)
     }
     
     static var allTests = [
-        ("testExample", testExample),
+        ("testRegular", testRegular),
+        ("testAE", testAE),
+        ("testOE", testOE),
+        ("testUnderscore", testUnderscore),
+        ("testSpecial_carrot", testSpecial_carrot),
+        ("testSpecial_backtick", testSpecial_backtick),
+        ("testSpecial_colon", testSpecial_colon),
+        ("testSpecial_euro", testSpecial_euro),
+        ("testSpace", testSpace),
+        ("testTile_hyphen", testTile_hyphen),
+        ("testTile_comma_hyphen_colon", testTile_comma_hyphen_colon)
+
     ]
 }
+
